7b3074e413272ec70b03d5e0dc56a2ceac2c18b8
LadyMarg <adeogunmargaret@gmail.com>
2021-10-12 18:55:06 +0100

    Marg Lady first commit to github

---
+++ src/Example.java
@@ -0,0 +1,7 @@
+public class Example {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Margaret Opeoluwa");
+        System.out.println("My github username is @margaret");
+    }
+}
